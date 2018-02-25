bfcb6a60c90a2f952168577dc5d98ea8b2c270de
Varun Chadha <vchadha023@gmail.com>
2018-02-25 13:16:47 -0600

    readded database file - vchadha

---
+++ javaScriptFiles/database.js
@@ -0,0 +1,31 @@
+function createEvent(eventObj) {
+    'use strict';
+    let request;
+    let eventData = JSON.stringify(eventObj);
+    
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbx5YzphchZuPi_D0U8FsleFgj8ngyDYb_nOkRCLh9iDZz4louOk/exec",
+        type: "post",
+        data: {
+            'EventDate' : eventObj["EventDate"],
+            'EventName' : eventObj["EventName"],
+            'EventData' : eventData
+        }
+    });
+}
+
+function createUser(userObj) {
+    'use strict';
+    let request;
+    let userData = JSON.stringify(userData);
+    
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbzft_iTCdt-tMZVntY38OVfU-Q-UIXYjP-kja7xm1iGVNJQ10w0/exec",
+        type: "post",
+        data: {
+            'Username' : userObj["username"],
+            'Password' : userObj["password"],
+            'isAdmin' : userData
+        }
+    });
+}
