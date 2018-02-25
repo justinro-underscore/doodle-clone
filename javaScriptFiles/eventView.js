896a0f87f1da8a0fe1f8b04f6b74c04777a49171
harryl6798 <harryl6798@gmail.com>
2018-02-25 16:52:44 -0600

    sdfsdfasdfasdfasdf

---
+++ javaScriptFiles/eventView.js
@@ -0,0 +1,52 @@
+
+// let createCharts = () => {
+//     google.charts.load('current', {
+//         packages: ['corechart', 'table', 'sankey']
+//     });
+//
+//     // Set a callback to run when the Google Visualization API is loaded.
+//     google.charts.setOnLoadCallback(drawChart);
+//
+//     function drawChart() {
+//         var query = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1f7v5k0XmaXdCAGkO9DGnv6uTzkAl_FjHHpg4jV9lTgI/edit#gid=0");
+//         query.send(handleQueryResponse);
+//     }
+//
+//     function handleQueryResponse(response) {
+//         var data = response.getDataTable();
+//         //TODO: for each of these we add to events obj
+//         //On create new event add to event obj && push event to sheets
+//         console.log(data.hc);
+//         var table = new google.visualization.Table(document.getElementById('table_div'));
+//
+//
+//         data.hideColumns(2);
+//         table.draw(data, {
+//
+//             showRowNumber: true,
+//             width: '100%',
+//             height: '100%'
+//         });
+//
+//         google.visualization.events.addListener(table, 'select', function () {
+//             let row = table.getSelection()[0].row;
+//             alert('You selected ' + data.getValue(row, 1));
+//         });
+//     }
+// };
+function listOfEvents()
+{
+  console.log(events);
+  // for(let i in events)
+  // {
+  //   console.log(events[i]);
+  // }
+  let eventsList = document.getElementById('eventsList');
+  let theEvents = document.createElement("a");
+  theEvents.setAttribute("href", "#");
+  theEvents.setAttribute("onclick", "theEvent()");
+  theEvents.setAttribute("id", id);
+  theEvents.innerHTML = id;
+  eventsList.append(events);
+}
+// createCharts();
