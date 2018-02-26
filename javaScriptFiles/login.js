0815cdddffc2cec0f9fe85c2797f07a1b027c85c
Alex Kunz <a009k781@ku.edu>
2018-02-26 08:50:41 -0600

    Add login page formatting
    The only functionality, as of now, is taking the user to the create
    user page if they so choose by clicking the create user button.

---
+++ javaScriptFiles/login.js
@@ -0,0 +1,7 @@
+function login() {
+    //access the correct user object(?)
+    //getuser using the the getuser function
+    //make sure password the same
+    //send them to index.html
+    //store the admin value of the user who logs in and who the person is to see who created the events
+}
